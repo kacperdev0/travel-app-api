2a4df1fbf3ec63378af748a9ff99d3d78aa2974a
kacperdev0 <kacperoldziejewski.kontakt@gmail.com>
2024-10-18 18:20:20 +0200

    made integration test for saving user

--- src/test/java/com/kacper/travelApp/service/UserServiceTest.java
+++ src/test/java/com/kacper/travelApp/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.kacper.travelApp.service;
 
 import com.kacper.travelApp.model.User;
 import com.kacper.travelApp.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,30 +10,37 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Transactional // Automatically rolls back transactions after each test method
 public class UserServiceTest {
-    @Mock
+
+    @Autowired
     private UserRepository userRepository;
 
-    @InjectMocks
-    private UserServiceImpl userService;
-
     @Test
-    public void saveUser_WhenCalled_ReturnsSavedUser() {
+    public void saveUser_WhenCalled_UserShouldBePresentInDatabase() {
         User testUser = User.builder()
                 .login("login1")
                 .username("username1")
                 .password("password1")
                 .email("email1@email.com")
-                .avatarUrl("link.link1").build();
+                .avatarUrl("link.link1")
+                .build();
 
         userRepository.save(testUser);
 
         User savedUser = userRepository.findByLogin(testUser.getLogin());
-        Assertions.assertThat(savedUser).isNotNull();
-    }
 
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getLogin()).isEqualTo(testUser.getLogin());
+        Assertions.assertThat(savedUser.getEmail()).isEqualTo(testUser.getEmail());
+
+        System.out.println("Test passed");
+    }
 }
